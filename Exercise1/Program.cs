148dd773d693e584ca43230a248053abaea46a7c
STAVROS KYRIACOU <101610510@student.swin.edu.au>
2021-04-15 14:45:52 +1000

    initial commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercide 1
+            string[] names = new string[5];
+
+            for (var i = 0; i < names.Length; i++)
+            {
+                Console.Write("Enter a name: ");
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("------------------------------");
+
+            for (var i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("Hello {0}", names[i]);
+            }
+        }
+    }
+}
