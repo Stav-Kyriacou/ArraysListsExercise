741db5ede9e950ca08414c890e75e4bc375bc882
STAVROS KYRIACOU <101610510@student.swin.edu.au>
2021-04-15 15:24:36 +1000

    initial commit

---
+++ Exercise5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 5
+            List<string> words = new List<string>();
+            string input = "";
+            
+            while(input != "stop")
+            {
+                Console.Write("Enter a word: ");
+                input = Console.ReadLine();
+
+                if(input != "stop")
+                {
+                    words.Add(input);
+                }
+            }
+            Console.WriteLine("Stopped");
+        }
+    }
+}
