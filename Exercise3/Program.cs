ac1fd28b2bae3027e945687a8b19bed26f2f3c69
STAVROS KYRIACOU <101610510@student.swin.edu.au>
2021-04-15 14:57:59 +1000

    initial commit

---
+++ Exercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            
+        }
+    }
+}
