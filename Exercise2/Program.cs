4d4ef3718b33488221fadf07aadd3ec1bb22c230
STAVROS KYRIACOU <101610510@student.swin.edu.au>
2021-04-15 14:55:15 +1000

    code + comments

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -6,12 +6,15 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
+            //Exercise 2
             int[] numbers = {34,5,67,1,99,34,44,78,34,0};
+            int total = 0;
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                
+                total += numbers[i];
             }
+            Console.WriteLine("The total is: {0}", total);
         }
     }
 }
