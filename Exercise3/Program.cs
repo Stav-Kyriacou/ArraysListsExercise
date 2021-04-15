49ad8a8d7e538238d12533cb0213d3fcfc8658f7
STAVROS KYRIACOU <101610510@student.swin.edu.au>
2021-04-15 15:08:05 +1000

    add code

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise3
 {
@@ -6,9 +7,28 @@ namespace Exercise3
     {
         static void Main(string[] args)
         {
+            //Exercise 3
             List<int> numbers = new List<int>();
+            int numLoops = 5;
 
-            
+            for (int i = 0; i < numLoops; i++)
+            {
+                Console.Write("Enter a number: ");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("---------------");
+            Console.Write("Enter another number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (numbers.Contains(input))
+            {
+                Console.WriteLine("{0} was already entered", input);   
+            }
+            else
+            {
+                Console.WriteLine("{0} was NOT previously entered", input);
+            }
         }
     }
 }
