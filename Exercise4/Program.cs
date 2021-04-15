29fc7ed0af2fb0cc8980d5b19ca491304dbe7634
STAVROS KYRIACOU <101610510@student.swin.edu.au>
2021-04-15 15:19:02 +1000

    finish task

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise4
 {
@@ -7,6 +8,29 @@ namespace Exercise4
         static void Main(string[] args)
         {
             //Exercise 4
+            List<int> numbers = new List<int>();
+            int repeatAmount = 0;
+            int numLoops = 5;
+
+            for (int i = 0; i < numLoops; i++)
+            {
+                Console.Write("Enter a number: ");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("---------------");
+            Console.Write("Enter another number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == input)
+                {
+                    repeatAmount++;
+                }
+            }
+
+            Console.WriteLine("The number {0} was repeated {1} time(s)", input, repeatAmount);
         }
     }
 }
